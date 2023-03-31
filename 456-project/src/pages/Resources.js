951efc54d59bb345ce0be2ba7a0c13cb185d348b
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2023-03-30 23:01:34 -0500

    Resources Links Updated
    Updated the links in Resources and integrated the Sound component for each link

--- 456-project/src/pages/Resources.js
+++ 456-project/src/pages/Resources.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import ResourceTitle from '../panels/ResourceTitle.js';
-import LinkOne from '../panels/LinkOne.js';
-import LinkTwo from '../panels/LinkTwo.js';
-import LinkThree from '../panels/LinkThree.js';
-import LinkFour from '../panels/LinkFour.js';
-import ReturnArrow from '../panels/ReturnArrow.js';
-import BackArrow from '../images/BackArrow.png';
-import ReturnHome from '../audio/ReturnHome.m4a';
 import Sound from '../panels/Sound.js';
-
+import ReturnHome from '../audio/ReturnHome.m4a';
 import Link1 from '../audio/Link1.m4a';
+import Link2 from '../audio/Link2.m4a';
+import Link3 from '../audio/Link3.m4a';
+import Link4 from '../audio/Link4.m4a';
+import BackArrow from '../images/BackArrow.png';
 import Link1Panel from '../images/Link1Panel.png';
+import Link2Panel from '../images/Link2Panel.png';
+import Link3Panel from '../images/Link3Panel.png';
+import Link4Panel from '../images/Link4Panel.png';
 
 function Resources() {
     return (
@@ -24,29 +24,29 @@ function Resources() {
             <div className="row">
                 <div className="column3">
                     <Link to="https://ridekc.org/rider-guide/navigator" target="_blank" rel="noreferrer">
-                        <Sound audioClip={Link1} source={Link1Panel} alternate="RideKC Navigator" />
+                        <SoundOne audioClip={Link1} source={Link1Panel} alternate="RideKC Navigator" />
                     </Link>
                 </div>
                 <div className="column3">
-                    <a href="https://ridekc.org/routes" target="_blank" rel="noreferrer">
-                        <LinkTwo />
-                    </a>
+                    <Link to="https://ridekc.org/routes" target="_blank" rel="noreferrer">
+                        <SoundOne audioClip={Link2} source={Link2Panel} alternate="RideKC Routes" />
+                    </Link>
                 </div>
                 <div className="column3">
-                    <a href="https://ridekc.org/fares/transit-app" target="_blank" rel="noreferrer">
-                        <LinkThree />
-                    </a>
+                    <Link to="https://ridekc.org/fares/transit-app" target="_blank" rel="noreferrer">
+                        <SoundOne audioClip={Link3} source={Link3Panel} alternate="RideKC Transit App" />
+                    </Link>
                 </div>
             </div>
             <div className="row">
                 <div className="column2">
-                    <a href="https://www.afb.org/blindness-and-low-vision/using-technology/assistive-technology-products/screen-readers" target="_blank" rel="noreferrer">
-                        <LinkFour />
-                    </a>
+                    <Link to="https://www.afb.org/blindness-and-low-vision/using-technology/assistive-technology-products/screen-readers" target="_blank" rel="noreferrer">
+                        <SoundOne audioClip={Link4} source={Link4Panel} alternate="Screen Reader Options" />
+                    </Link>
                 </div>
                 <div className="column2">
                     <Link to="/">
-                        <Sound audioClip={ReturnHome} source={BackArrow} alternate="Back" />
+                        <SoundOne audioClip={ReturnHome} source={BackArrow} alternate="Back" />
                     </Link>
                 </div>
             </div>
