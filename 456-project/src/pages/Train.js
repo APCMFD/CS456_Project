a182f3c83ab89653e593b7f7cc7a22b9dce49566
APCMFD <78195894+APCMFD@users.noreply.github.com>
2023-03-31 17:19:32 -0500

    update audio files and add tram and train pages

--- 456-project/src/pages/Train.js
+++ 456-project/src/pages/Train.js
@@ -1,14 +1,48 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import HomePageTitle from '../images/HomePageTitle.png';
+import TrainPageTitle from '../images/TrainPageTitle.png';
+import TutorialPanelImage from '../images/TutorialPanel.png';
+import AdditionalInfoImage from '../images/AdditionalInformationPanel.png';
+import TrainScheduleImage from '../images/TrainSchedulesPanel.png';
+import HomePageImage from '../images/BackArrow.png';
+import SoundOne from '../panels/SoundOne.js';
+import SoundTwo from '../panels/SoundTwo.js';
+import TrainPageTop from '../audio/TrainPageTop.m4a';
+import TrainInfo from '../audio/TrainInfo.m4a';
+import TrainTutorial from '../audio/TrainTutorial.m4a';
+import ReturnHome from '../audio/ReturnHome.m4a';
+import TrainSchedules from '../audio/TrainSchedules.m4a';
+import Hover19 from '../audio/Hover19.m4a';
+import Hover20 from '../audio/Hover20.m4a';
 
 const Train = () => {
     return (
         <>
             <div className="row">
                 <div className="column1">
-                    <img src={HomePageTitle} alt="Home Title" />
+                    <Link to="/">
+                        <SoundOne audioClip={TrainPageTop} source={TrainPageTitle} alternate="Train Page" />
+                    </Link>
                 </div>
+            </div>
+
+            <div className='row'>
+                    <div className='column2'>
+                        <SoundTwo audioClip={Hover19} audioClip2={TrainTutorial} source={TutorialPanelImage} alternate="Train Tutorial" />
+                    </div>
+                    <div className='column2'>
+                        <SoundTwo audioClip={Hover20} audioClip2={TrainInfo} source={AdditionalInfoImage} alternate="Additonal Train Information" />
+                    </div>
+                    <div className='column2'>
+                        <Link to="/trainSchedules">
+                            <SoundOne audioClip={TrainSchedules} source={TrainScheduleImage} alternate="Train Schedules" />
+                        </Link>
+                    </div>
+                    <div className='column2'>
+                        <Link to="/">
+                            <SoundOne audioClip={ReturnHome} source={HomePageImage} alternate="Back" />
+                        </Link>
+                    </div>
             </div>
         </>
     );
--- 456-project/src/pages/Tram.js
+++ 456-project/src/pages/Tram.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import HomePageTitle from '../images/HomePageTitle.png';
+import TramPageTitle from '../images/TramPageTitle.png';
+import TutorialPanelImage from '../images/TutorialPanel.png';
+import AdditionalInfoImage from '../images/AdditionalInformationPanel.png';
+import TramScheduleImage from '../images/StreetcarSchedulePanel.png';
+import HomePageImage from '../images/BackArrow.png';
+import SoundOne from '../panels/SoundOne.js';
+import SoundTwo from '../panels/SoundTwo.js';
+import TramPageTop from '../audio/TramPageTop.m4a';
+import TramInfo from '../audio/TramInfo.m4a';
+import TramTutorial from '../audio/TramTutorial.m4a';
+import ReturnHome from '../audio/ReturnHome.m4a';
+import TramSchedules from '../audio/TramSchedule.m4a';
+import Hover16 from '../audio/Hover16.m4a';
+import Hover17 from '../audio/Hover17.m4a';
+import Hover18 from '../audio/Hover18.m4a';
 
 const Tram = () => {
     return (
         <>
             <div className="row">
                 <div className="column1">
-                    <img src={HomePageTitle} alt="Home Title" />
+                    <Link to="/">
+                        <SoundOne audioClip={TramPageTop} source={TramPageTitle} alternate="Tram Page" />
+                    </Link>
                 </div>
+            </div>
+
+            <div className='row'>
+                    <div className='column2'>
+                        <SoundTwo audioClip={Hover16} audioClip2={TramTutorial} source={TutorialPanelImage} alternate="Tram Tutorial" />
+                    </div>
+                    <div className='column2'>
+                        <SoundTwo audioClip={Hover17} audioClip2={TramInfo} source={AdditionalInfoImage} alternate="Additonal Tram Information" />
+                    </div>
+                    <div className='column2'>
+                        <SoundTwo audioClip={Hover18} audioClip2={TramSchedules} source={TramScheduleImage} alternate="Tram Schedules" />
+                    </div>
+                    <div className='column2'>
+                        <Link to="/">
+                            <SoundOne audioClip={ReturnHome} source={HomePageImage} alternate="Back" />
+                        </Link>
+                    </div>
             </div>
         </>
     );
