c18711dfb3b4fc94fa15ddfc806c56f5ce8972ee
crjpb3 <60162347+crjpb3@users.noreply.github.com>
2023-03-30 22:42:36 -0500

    Corrected Sound component parameters
    Undid the portion of code in Sound.js that fixed the image issue. This version is properly working

--- 456-project/src/panels/Sound.js
+++ 456-project/src/panels/Sound.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import useSound from 'use-sound';
 
-const Sound = ({ audioClip }, source, alternate) => {
+const Sound = ({ audioClip , source, alternate}) => {
     
     const [play, { stop }] = useSound(audioClip);
   
     return (
-        <img style={{ width: "100%" }}src={source} alt={alternate} onMouseEnter={() => play()} onMouseLeave={() => stop()} />
+            <img style={{ width: "100%" }}src={source} alt={alternate} onMouseEnter={() => play()} onMouseLeave={() => stop()} />
     );
 }
 
